44adb2cae2d2ac4f87c9594fbdcd8d195849492f
Boris Kuchin <bvkuchin@gmail.com>
2023-02-08 23:47:08 +0300

    added SOAP search for products

--- src/main/java/ru/bvkuchin/market/Market/repositories/ProductRepository.java
+++ src/main/java/ru/bvkuchin/market/Market/repositories/ProductRepository.java
@@ -1,10 +1,20 @@
 package ru.bvkuchin.market.Market.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.bvkuchin.market.Market.entities.Product;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+//    @Query("select p from Product p where p.name = ?1")
+    Optional<Product> findByName(String name);
+
+    @Query("select p from Product p")
+    List<Product> getAllProducts();
 
 }
--- src/main/java/ru/bvkuchin/market/Market/services/ProductServise.java
+++ src/main/java/ru/bvkuchin/market/Market/services/ProductServise.java
@@ -3,10 +3,14 @@ package ru.bvkuchin.market.Market.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.bvkuchin.market.Market.entities.Product;
+import ru.bvkuchin.market.Market.exceptions.ResourceNotFoundException;
 import ru.bvkuchin.market.Market.repositories.ProductRepository;
+import ru.bvkuchin.market.Market.soap.products.ProductSoap;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,4 +29,22 @@ public class ProductServise {
     public Optional<Product> findById(Long id) {
         return productRepository.findById(id);
     }
+
+    private Function<Product, ProductSoap> productEntityToSoapConverter = product -> {
+        ProductSoap ps = new ProductSoap();
+        ps.setId(product.getId());
+        ps.setPrice(product.getPrice());
+        ps.setName(product.getName());
+        return ps;
+    };
+
+
+    public ProductSoap getByName(String name) {
+        return productRepository.findByName(name).map(productEntityToSoapConverter).orElseThrow(() -> new ResourceNotFoundException("No productSoap with name " + name));
+    }
+
+    public List<Product> getAllStudents() {
+        return productRepository.getAllProducts();
+    }
+
 }
