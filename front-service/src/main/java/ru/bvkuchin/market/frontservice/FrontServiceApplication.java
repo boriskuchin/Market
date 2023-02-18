f9071bd791640c492abe50422f1a405831da6c25
Boris Kuchin <bvkuchin@gmail.com>
2023-02-18 13:13:50 +0300

    externalized fromt service

---
+++ front-service/src/main/java/ru/bvkuchin/market/frontservice/FrontServiceApplication.java
@@ -0,0 +1,16 @@
+package ru.bvkuchin.market.frontservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontServiceApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontServiceApplication.class, args);
+    }
+
+
+}
